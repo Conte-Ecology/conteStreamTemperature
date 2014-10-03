0ae05cd7951fbd8de3cb398237d39ecb7d88b37f
Daniel Hocking <dhocking@unh.edu>
2014-10-03 12:22:23 -0400

    added westbook jags analysis function

--- R/jagsWrappers.R
+++ R/jagsWrappers.R
@@ -4,11 +4,9 @@
 #' \code{modelRegionalTempHUC} Linear mixed model in JAGS to model daily stream temperature
 #'
 #' @param data dataframe created in the 3-statModelPrep.Rmd script
-#' @param fixed.list List of fixed effects parameter names from columns in data. These are parameters without random slopes.
-#' @param random1 Character name of random variable 1 (e.g. site)
-#' @param random2 Character name of random variable 2 (e.g. year)
-#' @param random1.list List of names of parameters with random slopes by random variable 1
-#' @param random2.list List of names of parameters with random slopes by random variable 2
+#' @param data.fixed Dataframe of fixed effects parameter names from columns in data. These are parameters without random slopes.
+#' @param data.random.sites Dataframe of variables to have random slopes by site
+#' @param data.random.years Dataframe of variables to have random slopes by year
 #' @param params Character string of parameters to monitor (return) from the model
 #' @param n.burn Integer number of iterations in the burn-in (adapation) phase of the MCMC
 #' @param n.it Integer number of iterations per chain to run after the burn-in
@@ -18,15 +16,15 @@
 #' 
 #' @return Returns the iterations from the Gibbs sampler for each variable in params as either an mcmc.list or jags.samples object
 #' @details
-#' This function takes daily observed stream temperatures, air temperature, snow-water-equivalent (swe), day of the year, and landscape covariates for a linear mixed effects model with site and year random effects.
+#' This function takes daily observed stream temperatures, air temperature, day of the year, and landscape covariates for a linear mixed effects model with site within HUC8 and year random effects.
 #' 
 #' @examples
 #' 
 #' \dontrun{
-#' mcmc.out <- modelRegionalTemp(tempDataSyncS, n.burn = 1000, n.it = 1000, n.thin = 3, n.chains = 3)
+#' M.huc <- modelRegionalTempHUC(data, data.fixed, data.random.sites, data.random.years, n.burn = 1000, n.it = 1000, n.thin = 1, nc = 3, coda = coda.tf, param.list = monitor.params)
 #' }
 #' @export
-modelRegionalTempHUC <- function(data = tempDataSyncS, param.list, n.burn = 5000, n.it = 3000, n.thin = 3, nc = 3, coda = FALSE, runParallel = TRUE) {
+modelRegionalTempHUC <- function(data = tempDataSyncS, data.fixed, data.random.sites, data.random.years, param.list, n.burn = 5000, n.it = 3000, n.thin = 3, nc = 3, coda = FALSE, runParallel = TRUE) {
   #  temp.model <- function(){
 {
   sink("code/modelRegionalTempHUC.txt")
@@ -108,17 +106,9 @@ modelRegionalTempHUC <- function(data = tempDataSyncS, param.list, n.burn = 5000
 } # sink needs to be wrapped in expression for knitr to work
 
 # Fixed effects
-library(dplyr)
+#library(dplyr)
 
-  X.0 <- data.frame(intercept = 1,
-                    lat = data$Latitude,
-                    lon = data$Longitude,
-                    drainage = data$TotDASqKM,
-                    forest = data$Forest,
-                    elevation = data$ReachElevationM,
-                    coarseness = data$SurficialCoarseC,
-                    wetland = data$CONUSWetland,
-                    impoundments = data$ImpoundmentsAllSqKM)
+X.0 <- data.fixed
   variables.fixed <- names(X.0)
   K.0 <- length(variables.fixed)
 
@@ -134,13 +124,7 @@ library(dplyr)
 
 # Slope, Aspect, Dams/Impoundments, Agriculture, Wetland, Coarseness, dayl, srad, swe
 
-X.site <- data.frame(intercept.site = 1, 
-                     airTemp = data$airTemp, 
-                     #airTempLag1 = data$airTempLagged1,
-                     airTempLag2 = data$airTempLagged2,
-                     precip = data$prcp,
-                     precipLag1 = data$prcpLagged1,
-                     precipLag2 = data$prcpLagged2) # , swe = data$swe
+X.site <- data.random.sites
 variables.site <- names(X.site)
 J <- length(unique(data$site))
 K <- length(variables.site)
@@ -156,10 +140,7 @@ W.huc <- diag(K)
 #                 "dOY2",
 #                "dOY3")
 
-X.year <- data.frame(intercept.year = 1, 
-                     dOY = data$dOY, 
-                     dOY2 = data$dOY^2,
-                     dOY3 = data$dOY^3)
+X.year <- data.random.years
 variables.year <- names(X.year)
 Ti <- length(unique(data$year))
 L <- length(variables.year)
@@ -278,6 +259,198 @@ if(runParallel) {
     library(rjags)
     load.module('glm')
     jm <- jags.model("code/modelRegionalTempHUC.txt", data.list, inits, n.adapt = n.burn, n.chains=nc)
+    fm <- jags.samples(jm, params, n.iter = n.it, thin = n.thin)
+    return(fm)
+  }
+}
+}
+
+#' @title modelRegionalTempWB
+#'
+#' @description
+#' \code{modelRegionalTempWB} Linear mixed model in JAGS to model daily stream temperature in the West Brook and it's tributaries
+#'
+#' @param data dataframe created in the 3-statModelPrep.Rmd script
+#' @param data.fixed Dataframe of fixed effects parameter names from columns in data. These are parameters without random slopes.
+#' @param data.random.years Dataframe of variables to have random slopes by year
+#' @param params Character string of parameters to monitor (return) from the model
+#' @param n.burn Integer number of iterations in the burn-in (adapation) phase of the MCMC
+#' @param n.it Integer number of iterations per chain to run after the burn-in
+#' @param n.thin Integer: save every nth iteration
+#' @param n.chains Integer number of chains. One run per cluster so should use <= # cores on computer
+#' @param coda Logical if TRUE return coda mcmc.list, if FALSE (default) return jags.samples object
+#' 
+#' @return Returns the iterations from the Gibbs sampler for each variable in params as either an mcmc.list or jags.samples object
+#' @details
+#' This function takes daily observed stream temperatures, air temperature, day of the year, and landscape covariates for a linear mixed effects model with site within HUC8 and year random effects.
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' M.wb <- modelRegionalTempWB(data=DF, data.fixed = DF.fixed, data.random.years = DF.years, n.burn = 1000, n.it = 1000, n.thin = 1, nc = 3, coda = coda.tf, param.list = monitor.params)
+#' }
+#' @export
+modelRegionalTempWB <- function(data = tempDataSyncS, data.fixed, data.random.years, param.list, n.burn = 5000, n.it = 3000, n.thin = 3, nc = 3, coda = FALSE, runParallel = TRUE) {
+  #  temp.model <- function(){
+{
+  sink("code/modelRegionalTempWB.txt")
+  cat("
+      model{
+      # Likelihood
+      for(i in 1:n){ # n observations
+      temp[i] ~ dnorm(stream.mu[i], tau)
+      stream.mu[i] <- inprod(B.0[], X.0[i, ]) + inprod(B.year[year[i], ], X.year[i, ]) #  
+      }
+      
+      # prior for model variance
+      sigma ~ dunif(0, 100)
+      tau <- pow(sigma, -2)
+      
+      for(k in 1:K.0){
+      B.0[k] ~ dnorm(0, 0.001) # priors coefs for fixed effect predictors
+      }
+      
+      # YEAR EFFECTS
+      # Priors for random effects of year
+      for(t in 1:Ti){ # Ti years
+      B.year[t, 1:L] ~ dmnorm(mu.year[ ], tau.B.year[ , ])
+      }
+      mu.year[1] <- 0
+      for(l in 2:L){
+      mu.year[l] ~ dnorm(0, 0.0001)
+      }
+      
+      # Prior on multivariate normal std deviation
+      tau.B.year[1:L, 1:L] ~ dwish(W.year[ , ], df.year)
+      df.year <- L + 1
+      sigma.B.year[1:L, 1:L] <- inverse(tau.B.year[ , ])
+      for(l in 1:L){
+      for(l.prime in 1:L){
+      rho.B.year[l, l.prime] <- sigma.B.year[l, l.prime]/sqrt(sigma.B.year[l, l]*sigma.B.year[l.prime, l.prime])
+      }
+      sigma.b.year[l] <- sqrt(sigma.B.year[l, l])
+      }
+      
+      # Derived parameters
+      for(i in 1:n) {
+      residuals[i] <- temp[i] - stream.mu[i]
+      }
+      }
+      ",fill = TRUE)
+  sink()
+} # sink needs to be wrapped in expression for knitr to work
+
+X.0 <- data.fixed
+variables.fixed <- names(X.0)
+K.0 <- length(variables.fixed)
+
+n <- dim(data)[1]
+
+X.year <- data.random.years
+variables.year <- names(X.year)
+Ti <- length(unique(data$year))
+L <- length(variables.year)
+W.year <- diag(L)
+
+data.list <- list(n = n,  
+                  Ti = Ti,
+                  L = L,
+                  K.0 = K.0,
+                  X.0 = X.0,
+                  W.year = W.year,
+                  temp = data$temp,
+                  X.year = as.matrix(X.year),
+                  site = as.factor(data$site),
+                  year = as.factor(data$year))
+
+inits <- function(){
+  list(#B.raw = array(rnorm(J*K), c(J,K)), 
+    #mu.site.raw = rnorm(K),
+    sigma = runif(1))
+  #tau.B.site.raw = rwish(K + 1, diag(K)),)
+}
+
+if(class(param.list) == "character") {
+  params <- param.list
+} else {
+  params <- c("sigma",
+              "B.0",
+              "B.huc",
+              "B.year",
+              "rho.B.year",
+              "mu.year",
+              "sigma.b.year",
+              "residuals")#,
+  # "stream.mu")
+}
+
+#M1 <- bugs(data, )
+
+# n.burn = 5000
+#  n.it = 3000
+# n.thin = 3
+
+library(parallel)
+library(rjags)
+
+if(nc) {
+  nc <- nc
+} else {
+  nc <- 3 
+}
+
+# model.tc <- textConnection(modelstring)
+#filename <- file.path(tempdir(), "tempmodel.txt")
+#write.model(temp.model, filename)
+
+if(runParallel) {
+  if(coda) {
+    CL <- makeCluster(nc)
+    clusterExport(cl=CL, list("data.list", "inits", "params", "Ti", "L", "n", "W.year", "X.site", "X.year", "n.burn", "n.it", "n.thin"), envir = environment())
+    clusterSetRNGStream(cl=CL, iseed = 2345642)
+    
+    system.time(out <- clusterEvalQ(CL, {
+      library(rjags)
+      load.module('glm')
+      jm <- jags.model("code/modelRegionalTempWB.txt", data.list, inits, n.adapt = n.burn, n.chains=1)
+      fm <- coda.samples(jm, params, n.iter = n.it, thin = n.thin)
+      return(as.mcmc(fm))
+    }))
+    
+    M3 <- mcmc.list(out)
+    
+    stopCluster(CL)
+    return(M3)
+    
+  } else {
+    CL <- makeCluster(nc)
+    clusterExport(cl=CL, list("data.list", "inits", "params", "Ti", "L", "n", "W.year", "X.site", "X.year", "n.burn", "n.it", "n.thin"), envir = environment())
+    clusterSetRNGStream(cl=CL, iseed = 2345642)
+    
+    system.time(out <- clusterEvalQ(CL, {
+      library(rjags)
+      load.module('glm')
+      jm <- jags.model("code/modelRegionalTempWB.txt", data.list, inits, n.adapt = n.burn, n.chains=1)
+      fm <- jags.samples(jm, params, n.iter = n.it, thin = n.thin)
+      return(fm)
+    }))
+    
+    stopCluster(CL)
+    return(out)
+  }
+  
+  
+} else {
+  if(coda) {
+    library(rjags)
+    load.module('glm')
+    jm <- jags.model("code/modelRegionalTempWB.txt", data.list, inits, n.adapt = n.burn, n.chains=nc)
+    fm <- coda.samples(jm, params, n.iter = n.it, thin = n.thin)
+    return(fm)
+  } else {
+    library(rjags)
+    load.module('glm')
+    jm <- jags.model("code/modelRegionalTempWB.txt", data.list, inits, n.adapt = n.burn, n.chains=nc)
     fm <- jags.samples(jm, params, n.iter = n.it, thin = n.thin)
     return(fm)
   }
