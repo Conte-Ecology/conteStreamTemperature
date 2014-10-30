b060fcf7027c0837a288767659adfbb6a547b771
Daniel Hocking <dhocking@unh.edu>
2014-10-29 21:43:46 -0400

    made wrapper for data prep to improve portability

--- R/Data_Prep_Functions.R
+++ R/Data_Prep_Functions.R
@@ -21,7 +21,7 @@ indexDeployments <- function(data, regional = FALSE) {
 #  data1=data.frame(site=rep(1:4,each=4),date=rep(1:4))
 #  data=rbind(data1,data1[15:16,])
   
-    data%>%
+    data %>%
       mutate( siteShift = c( 1,site[ 1:(nrow(data)-1) ] ),
               dateShift = c( 1,date[ 1:(nrow(data)-1) ] ),
               newSite = site == siteShift + 1,
@@ -63,4 +63,114 @@ createDeployRows <- function(data) {
 
 
 # this could go in another file. Ben stuck it here for now
-"%!in%" <- function(x,table) match(x,table, nomatch = 0) == 0
+"%!in%" <- function(x, table) match(x, table, nomatch = 0) == 0
+
+
+
+
+
+#' @title prepDataWrapper: Wrapper to prepare data for analysis or predictions
+#'
+#' @description
+#' \code{prepDataWrapper} Wrapper to prepare data for analysis or predictions
+#'
+#' @param 
+#' @details
+#' var: blah, blah, blah
+#' value: something, something
+#' @export
+prepDataWrapper <- function(data.fit = NULL, var.names, dataInDir, dataOutDir, predict.daymet = FALSE, validate = FALSE, validateFrac = NULL, ...) {
+  
+  tempData <- readStreamTempData(timeSeries=TRUE, covariates=TRUE, dataSourceList=dataSource, fieldListTS=fields, fieldListCD='ALL', directory=dataInDir)
+  
+  springFallBPs$site <- as.character(springFallBPs$site)
+  
+  if(predict.daymet) {
+    ########## How to add BP for years without data and clip data to the sync period ??? #######
+    # Join with break points
+    covariateDataBP <- left_join(covariateData, springFallBPs, by=c('site', 'year'))
+    # rm(covariateData)
+    
+    # temp hack
+    climateData$site <- as.character(climateData$site)
+    tempDataBP <- left_join(climateData, covariateDataBP, by=c('site'))
+    
+    # Clip to syncronized season
+    # tempFullSync <- filter(tempDataBP, dOY >= finalSpringBP & dOY <= finalFallBP)
+    
+    # temp hack
+    tempDataSync <- filter(tempDataBP, dOY >= 50 & dOY <= 350)
+    tempDataSync$Latitude <- tempDataSync$Latitude.x
+    tempDataSync$Longitude <- tempDataSync$Longitude.x
+    ##################
+  } else {
+    # Join with break points
+    tempDataBP <- left_join(tempData, springFallBPs, by=c('site', 'year'))
+    rm(tempData) # save some memory
+    
+    # Clip to syncronized season
+    tempDataSync <- filter(tempDataBP, dOY >= finalSpringBP & dOY <= finalFallBP)
+  }
+  
+  # Order by group and date
+  tempDataSync <- tempDataSync[order(tempDataSync$site,tempDataSync$year,tempDataSync$dOY),]
+  
+  # For checking the order of tempDataSync
+  tempDataSync$count <- 1:length(tempDataSync$year)
+  
+  tempDataSync <- tempDataSync[order(tempDataSync$count),] # just to make sure tempDataSync is ordered for the slide function
+  
+  # airTemp
+  tempDataSync <- slide(tempDataSync, Var = "airTemp", GroupVar = "site", slideBy = -1, NewVar='airTempLagged1')
+  tempDataSync <- slide(tempDataSync, Var = "airTemp", GroupVar = "site", slideBy = -2, NewVar='airTempLagged2')
+  
+  # prcp
+  tempDataSync <- slide(tempDataSync, Var = "prcp", GroupVar = "site", slideBy = -1, NewVar='prcpLagged1')
+  tempDataSync <- slide(tempDataSync, Var = "prcp", GroupVar = "site", slideBy = -2, NewVar='prcpLagged2')
+  tempDataSync <- slide(tempDataSync, Var = "prcp", GroupVar = "site", slideBy = -3, NewVar='prcpLagged3')
+  
+  # Make dataframe with just variables for modeling and order before standardizing
+  tempDataSync <- tempDataSync[ , c("agency", "date", "AgencyID", "year", "site", "date", "finalSpringBP", "finalFallBP", "FEATUREID", "HUC4", "HUC8", "HUC12", "temp", "Latitude", "Longitude", "airTemp", "airTempLagged1", "airTempLagged2", "prcp", "prcpLagged1", "prcpLagged2", "prcpLagged3", "dOY", "Forest", "Herbacious", "Agriculture", "Developed", "TotDASqKM", "ReachElevationM", "ImpoundmentsAllSqKM", "HydrologicGroupAB", "SurficialCoarseC", "CONUSWetland", "ReachSlopePCNT", "srad", "dayl", "swe")] #  
+  
+  tempDataSync <- filter(tempDataSync, filter = TotDASqKM <= filter.area)
+  
+  ### Separate data for fitting (training) and validation
+  
+  #If validating:
+  if(validate) {
+    n.fit <- floor(length(unique(tempDataSync$site)) * (1 - validateFrac))
+    
+    set.seed(2346)
+    site.fit <- sample(unique(tempDataSync$site), n.fit, replace = FALSE) # select sites to hold back for testing 
+    tempDataSyncValid <- subset(tempDataSync, !site %in% site.fit) # data for validation
+    tempDataSync <- subset(tempDataSync, site %in% site.fit)    # data for model fitting (calibration)
+    
+    tempDataSyncValidS <- stdCovs(x = tempDataSyncValid, y = tempDataSync, var.names = var.names)
+    
+    tempDataSyncValidS <- indexDeployments(tempDataSyncValidS, regional = TRUE)
+    rows.valid <- createDeployRows(tempDataSyncValidS)
+    firstObsRowsValid <- rows.valid$firstObsRows
+    evalRowsValid <- rows.valid$evalRows
+    
+  } else {
+    tempDataSyncValid <- NULL
+  }
+  
+  # Standardize for Analysis
+  
+  tempDataSyncS <- stdFitCovs(x = tempDataSync, var.names = var.names)
+  
+  ### Add unique deployment column and create vector to loop through for each unique site-deployment
+  
+  # Data for fitting
+  tempDataSyncS <- indexDeployments(tempDataSyncS, regional = TRUE)
+  rows <- createDeployRows(tempDataSyncS)
+  firstObsRows <- rows$firstObsRows
+  evalRows <- rows$evalRows
+  
+  if(validate) {
+    save(tempDataSync, tempDataSyncS, tempDataSyncValid, tempDataSyncValidS, firstObsRows, evalRows, firstObsRowsValid, evalRowsValid, file = paste0(dataOutDir, 'tempDataSync.RData'))
+  } else {
+    save(tempDataSync, tempDataSyncS, firstObsRows, evalRows, file = paste0(dataOutDir, 'tempDataSync.RData'))
+  }
+}
--- R/stdCovs.R
+++ R/stdCovs.R
@@ -9,10 +9,30 @@
 #' This function standardizes the list of covariates in the new dataframe (x) using the means and standard deviations from the dataframe (y) that was used to fit (calibrate) the model
 #' @export
 stdCovs <- function(x, y, var.names){
-  xStd <- as.data.frame(matrix(NA, dim(x)[1], length(var.names)))
-  names(xStd) <- var.names
   for(i in 1:length(var.names)){
-    xStd[ , var.names[i]] <- (x[ , var.names[i]] - mean(y[ , var.names[i]], na.rm=T)) / sd(y[ , var.names[i]], na.rm=T)
+    x[ , var.names[i]] <- (x[ , var.names[i]] - mean(y[ , var.names[i]], na.rm=T)) / sd(y[ , var.names[i]], na.rm=T)
   }
-  return(xStd)
+  return(x)
 }
+
+
+#' @title Standardize covariates for model fitting
+#'
+#' @description
+#' \code{stdFitCovs} returns a dataframe of the standardized values of all the variables in x.
+#' @param x Dataframe of covariates for prediction or validation
+#' @param var.names Character vector of covariate names common in x and y to standardize
+#' @details
+#' This function standardizes the list of covariates in the new dataframe (x) using the means and standard deviations from the dataframe (y) that was used to fit (calibrate) the model
+#' @export
+stdFitCovs <- function(x, var.names){
+  for(i in 1:length(var.names)){
+    x[ , var.names[i]] <- (x[ , var.names[i]] - mean(x[ , var.names[i]], na.rm = T)) / sd(x[ , var.names[i]], na.rm = T)
+  }
+  return(x)
+}
+
+
+
+
+
