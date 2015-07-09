05f75d99d9edf9a5daec1cbea460993c349d20cb
Daniel Hocking <dhocking@unh.edu>
2015-07-09 15:20:50 -0400

    Changed standardization to use means and stds rather than recalculating them each time.

--- R/stdCovs.R
+++ R/stdCovs.R
@@ -3,14 +3,14 @@
 #' @description
 #' \code{stdCovs} returns a dataframe of the standardized values of all the variables in x.
 #' @param x Dataframe of covariates for prediction or validation
-#' @param y Dataframe used for model fitting
+#' @param y Dataframe of means and standard deviations
 #' @param var.names Character vector of covariate names common in x and y to standardize
 #' @details
 #' This function standardizes the list of covariates in the new dataframe (x) using the means and standard deviations from the dataframe (y) that was used to fit (calibrate) the model
 #' @export
 stdCovs <- function(x, y, var.names){
   for(i in 1:length(var.names)){
-    x[ , var.names[i]] <- (x[ , var.names[i]] - mean(y[ , var.names[i]], na.rm=T)) / sd(y[ , var.names[i]], na.rm=T)
+    x[ , var.names[i]] <- (x[ , var.names[i]] - y[which(var.names == var.names[i]), "means"]) / y[which(var.names == var.names[i]), "stdevs"]
   }
   return(x)
 }
